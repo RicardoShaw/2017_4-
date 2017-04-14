dfaea32d4a1c86b85d7d87acdc7cf44676f6b5fa
RicardoShaw <RicardoShaw@qq.com>
2017-04-14 18:01:04 +0800

    Change-Id: Iedcb535750196362c5e42c8815a67e2b9268e50c Signed-off-by: RicardoShaw <RicardoShaw@qq.com>

---
+++ src/com/ricardo/object/Test4.java
@@ -0,0 +1,30 @@
+package com.ricardo.object;
+
+public class Test4 {
+	public static void main(String[] args){
+		System.out.println(digui(30));
+		
+
+		char a = '我';//�����������16λunicode����
+		char b = 'd';//Ӣ����һ���8λunicode����
+
+		
+		System.out.println();
+	}
+	
+	public static int digui(int i){
+		if(i == 0){
+			return 0;
+		}
+		
+		
+		if(i==1){
+			return 1;
+		}
+		
+		
+		int a = i-2;
+		int b = i-1;
+		return digui(a)+digui(b);
+	}
+}
